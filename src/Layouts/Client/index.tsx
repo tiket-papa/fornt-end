5c2ac85f2ba1e3be0e5f9cc344ef9b456770a6b1
masQ-21dev <mqomarudin0480@gmail.com>
2023-11-01 13:24:53 +0700

    responsive navbar

--- src/Layouts/Client/index.tsx
+++ src/Layouts/Client/index.tsx
@@ -4,9 +4,7 @@ import NavClient from '@/components/Clinet/Nav/Nav'
 export default function Client ({ children }: { children: React.ReactNode }) {
   return (
       <>
-          <header className='container mx-auto'>
-              <NavClient/>
-          </header>
+          <NavClient/>
           {children}
           <FooterClient/>
       </>
--- src/components/Clinet/Footer/Footer.tsx
+++ src/components/Clinet/Footer/Footer.tsx
@@ -14,8 +14,8 @@ export default function FooterClient () {
   const logo = '/Statick/image/logo_tiket_papa.png'
   return (
       <>
-          <div className='mt-10 mb-5 border-t-2'></div>
-          <footer className="footer container mx-auto">
+          <div className='mt-10 mb-5  border-t-2'></div>
+          <footer className="footer container mx-auto px-2">
               <div className="logo grid grid-cols-1 md:grid-cols-2 gap-10">
                   <div className="image h-10 md:h-16 relative">
                       <Image
@@ -23,6 +23,7 @@ export default function FooterClient () {
                           alt='logo tiket papa'
                           className='object-contain object-left'
                           fill={true}
+                          sizes='500px'
                           priority
                 />
                   </div>
@@ -30,7 +31,7 @@ export default function FooterClient () {
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci neque quaerat voluptate earum sequi eligendi molestias nisi amet minus nesciunt?</p>
                   </div>
               </div>
-              <div className="information grid grid-cols-5 my-10">
+              <div className="information grid grid-cols-2 md:grid-cols-5 gap-10 my-10">
                   <div className='information'>
                       <h1 className='font-bold'>Information</h1>
 
@@ -44,21 +45,20 @@ export default function FooterClient () {
                       <h1 className='font-bold'>Support</h1>
 
                       <div className="link-component text-gray-400 pt-5">
-                          <Link className='py-1 block' href={'/'}>About Us</Link>
-                          <Link className='py-1 block' href={'/'}>Privacy Police</Link>
-                          <Link className='py-1 block' href={'/'}>Terms and Condition</Link>
+                          <Link className='py-1 block' href={'/'}>Help Center</Link>
+                          <Link className='py-1 block' href={'/'}>Report a Bug</Link>
+                          <Link className='py-1 block' href={'/'}>Chat Support</Link>
                       </div>
                   </div>
                   <div className="contact">
                       <h1 className='font-bold'>Contact</h1>
 
                       <div className="link-component text-gray-400 pt-5">
-                          <Link className='py-1 block' href={'/'}>About Us</Link>
-                          <Link className='py-1 block' href={'/'}>Privacy Police</Link>
-                          <Link className='py-1 block' href={'/'}>Terms and Condition</Link>
+                          <a className='py-1 block' href={'/'}>About Us</a>
+                          <a className='py-1 block' href={'/'}>Privacy Police</a>
                       </div>
                   </div>
-                  <div className="office col-span-2">
+                  <div className="office md:col-span-2">
                       <div className="office">
                           <h1 className='font-bold'>Office</h1>
 
@@ -76,11 +76,11 @@ export default function FooterClient () {
 
                   </div>
               </div>
-              <div className="grid grid-cols-2 py-5 mt-6">
-                  <div className="text-gray-500">
+              <div className="grid md:grid-cols-2 py-5 mt-6">
+                  <div className="text-gray-500 text-center md:text-start">
                       <span>Copyright © 2023 TIKETPAPA | All Rights Reserved </span>
                   </div>
-                  <div className="flex gap-10 justify-end text-main-color text-2xl">
+                  <div className="flex gap-10 justify-center md:justify-end text-main-color text-2xl">
                       <a target='_blank' rel="noopener noreferrer" href={socialMedia.facebook}>
                           <FontAwesomeIcon icon={faSquareFacebook}/>
                       </a>
